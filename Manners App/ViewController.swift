3de195da9ddcdc7456ce489a45996e80114a6b83
Danny Corona <dancorona@live.com>
2016-04-14 12:44:16 -0700

    Initial Commit

---
+++ Manners App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Manners App
+//
+//  Created by Daniel Corona on 4/14/16.
+//  Copyright © 2016 Danny Corona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
